fd7edd8ad8cae64f2594074e0b7014df6ac86626
Brian Cajes <bcajes@jenkins.clipmerge.net>
2013-11-30 18:41:43 -0500

    testing travis

--- pipeable/tests/test_pipeline.py
+++ pipeable/tests/test_pipeline.py
@@ -24,8 +24,8 @@ class TestPipeline(unittest.TestCase):
     def test_should_run_simple_pipes_add_one_add_two(self):
         test_pipes = self._getSimpleTestPipes()
         pipe_line = self._makeOne(test_pipes)
-        res = pipe_line.run(1).next()
-        self.assertEquals(4, res, res)
+        res = list(pipe_line.run(1))
+        self.assertEquals([4], res, res)
 
     def test_should_run_simple_pipes_add_one_add_two_generator_input(self):
         test_pipes = self._getSimpleTestPipes()
@@ -98,10 +98,10 @@ class TestYamlPipelineConfigurator(unittest.TestCase):
 
     def test_should_create_pipeline_given_config_and_context(self):
         try:
-            from io import StringIO
-        except ImportError:
             from StringIO import StringIO
-        config = StringIO(unicode(self._sample_yaml_config_contents))
+        except ImportError:
+            from io import StringIO
+        config = StringIO(self._sample_yaml_config_contents)
         ctx = get_sample_context()
         yaml_configurator = self._getTargetClass()
         pipeline = yaml_configurator.createPipeline(config, ctx)
